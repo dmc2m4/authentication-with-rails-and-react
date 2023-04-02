1800918b68fff0d8f07e6d8445a325c684a5fb20
dmc2m4 <danielmartinezcabrera@hotmail.com>
2023-04-02 09:48:15 -0500

    cors ready for every domain

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*' # Change this to the domain(s) you want to allow requests from
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+  end
+  
